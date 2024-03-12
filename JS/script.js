8dd508c19c9c3707298cf7786eef6efd10c91ea4
DarleneLL <153782176+DarleneLL@users.noreply.github.com>
2024-03-12 16:19:22 -0300

    M1S08 exercicio 6

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+// Adicione uma nova classe a um elemento específico na página HTML.
+
+function adicionaNovaClasse(){
+    let classeOriginal = document.querySelector('h1.classeOriginal')
+    classeOriginal.classList.add('classeNova')
+}
